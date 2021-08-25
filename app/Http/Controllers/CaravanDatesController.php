9afa86af71fb0714f537a36393dc65583e289d31
berndengels <engels@f50.de>
2021-08-25 13:59:40 +0200

    stuff

---
+++ app/Http/Controllers/CaravanDatesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CaravanDates;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CaravanDatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param CaravanDates $caravanDates
+     * @return Response
+     */
+    public function show(CaravanDates $caravanDates)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param CaravanDates $caravanDates
+     * @return Response
+     */
+    public function edit(CaravanDates $caravanDates)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param CaravanDates $caravanDates
+     * @return Response
+     */
+    public function update(Request $request, CaravanDates $caravanDates)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param CaravanDates $caravanDates
+     * @return Response
+     */
+    public function destroy(CaravanDates $caravanDates)
+    {
+        //
+    }
+}
