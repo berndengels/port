ad604da1c395033a46ac8fa0fe1d7055c8717b30
berndengels <engels@f50.de>
2021-09-23 22:32:00 +0200

    stuff

--- resources/views/public/dashboard.blade.php
+++ resources/views/public/dashboard.blade.php
@@ -2,20 +2,24 @@
 
 @section('main')
     <div class="flex-container-dashboard public">
-        <div class="flex-item-dashbord p-3">
+
+        <div class="flex-item-dashboard p-3">
             <div class="">
                 Camerarius, emeritis itineris tramitems acceleratrix tractare de lotus, velox xiphias.
             </div>
         </div>
+
         <div class="flex-item-dashboard p-3">
             <div class="flex flex-wrap flex-shrink-0">
                 Die proudly like a remarkable particle.
             </div>
         </div>
+
         <div class="flex-item-dashboard p-3">
             <div class="flex flex-wrap flex-shrink-0">
                 Believers, lovers, and simple therapists will always protect them.
             </div>
         </div>
+
     </div>
 @endsection
