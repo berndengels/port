ab5df18b993ae66a452eac16ad3b4eeb5eee1b60
berndengels <engels@f50.de>
2021-09-23 23:06:35 +0200

    stuff

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -7,18 +7,23 @@ use Illuminate\Support\Facades\Route;
 class RouteController extends Controller
 {
     protected $route;
+    protected $startRoute;
     protected $currentName;
     protected $currentRoutes;
 
     public function setCurrentMenu(Request $request) {
-        $this->currentName = $request->input('current');
-        $this->route = $request->input('route');
-
-        $request->session()->put('currentName', $this->currentName);
-        $request->session()->put('currentRoutes', config('port.menu.admin.items.'.$this->currentName));
-
+        $this->route            = $request->input('route');
+        $this->currentName      = $request->input('current');
+        $this->currentRoutes    = config('port.menu.admin.items.'.$this->currentName);
+        $this->startRoute       = (!$this->route && count($this->currentRoutes['items']) && isset($this->currentRoutes['items'][0]['route'])) ? $this->currentRoutes['items'][0]['route'] : null;
         if($this->route && Route::getRoutes()->hasNamedRoute($this->route)) {
-            return redirect()->route($this->route);
+            $this->startRoute = $this->route;
+        }
+        $request->session()->put('currentName', $this->currentName);
+        $request->session()->put('currentRoutes', $this->currentRoutes);
+
+        if($this->startRoute) {
+            return redirect()->route($this->startRoute);
         } else {
             return redirect()->back();
         }
