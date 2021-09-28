76926bc0a90d64bd8b302f5801087d85098b90dd
berndengels <engels@f50.de>
2021-09-28 11:59:19 +0200

    stuff

--- resources/views/admin/caravanDates/create.blade.php
+++ resources/views/admin/caravanDates/create.blade.php
@@ -18,6 +18,7 @@
 				<x-form-checkbox class="calc" name="electric" label="Stromanschluß" />
 			</div>
 			<x-form-input class="calc" name="persons" label="Anzahl Personen" required />
+			<x-form-input name="day_price" label="Tages-Preis" />
 			<x-form-input name="price" label="Preis" required />
 			<div class="mt-2">
 				<x-form-submit class="btn btn-save h-10 mt-3 w-full md:w-1/2" icon="fas fa-save">Speichern</x-form-submit>
--- resources/views/admin/caravanDates/edit.blade.php
+++ resources/views/admin/caravanDates/edit.blade.php
@@ -9,7 +9,7 @@
             @bind($caravanDate->caravan)
             <x-form-input name="carnumber" label="Autokennzeichen" required />
             <x-form-select name="country_id" label="Herkunftsland" :options="$countries" />
-            <x-form-input name="carlength" label="Länge" required />
+            <x-form-input name="carlength" type="number" label="Länge" required />
             <x-form-input type="email" name="email" label="Email" />
             @endbind
 
@@ -19,8 +19,9 @@
             <div class="mt-3">
                 <x-form-checkbox class="calc" name="electric" label="Stromanschluß" />
             </div>
-            <x-form-input class="calc" name="persons" label="Anzahl Personen" required />
-            <x-form-input name="price" label="Preis" required />
+            <x-form-input class="calc" name="persons" type="number" label="Anzahl Personen" required />
+            <x-form-input name="day_price" type="number" label="Tages-Preis" />
+            <x-form-input name="price" type="number" label="Preis" required />
             @endbind
             <div class="mt-2">
                 <x-form-submit class="btn btn-save h-10 mt-3 w-full md:w-1/2" icon="fas fa-save">Speichern</x-form-submit>
