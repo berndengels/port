ef5dfad242ec73c926d42d90682aee342098efd0
berndengels <engels@f50.de>
2021-10-13 02:01:27 +0200

    stuff

--- resources/views/public/dashboard.blade.php
+++ resources/views/public/dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="flex-container-dashboard public">
         @if($widgets->count() > 0)
             @foreach($widgets as $item)
-            <div class="flex-item-dashboard p-3">
+            <div class="flex-item-dashboard p-3 widget">
                 <div class="title">{{ $item->title }}</div>
                 <div class="content mt-2">{!! $item->content !!}</div>
             </div>
