2b969193dd2c77886a5a13cf7e2ade72f1b4c995
berndengels <engels@f50.de>
2021-10-29 05:46:39 +0200

    stuff

--- resources/views/admin/boats/edit.blade.php
+++ resources/views/admin/boats/edit.blade.php
@@ -18,7 +18,7 @@
             <x-form-input name="boat_name" label="Boots Name" required />
             <x-form-input name="length" type="number" step="0.1" min="1" label="Boots Länge" required />
             <x-form-input name="width" type="number" step="0.1"  min="1" label="Boots Breite" />
-            <x-form-input name="weight" type="number" step="100" min="1" label="Boots Gewicht in Kg" placeholder="Gewicht in Kilogramm" />
+            <x-form-input name="weight" type="number" min="1" label="Boots Gewicht in Kg" placeholder="Gewicht in Kilogramm" />
             <x-form-input name="mast_length" type="number" step="1" min="0" label="Mastlänge" />
             <x-form-input name="mast_weight" type="number" step="1" min="0" label="Mastgewicht in Kg" placeholder="Gewicht in Kilogramm" />
             <x-form-input name="draft" type="number" step="0.1" min="0.1" label="Tiefgang" />
