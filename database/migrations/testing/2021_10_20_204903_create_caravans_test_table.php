c1a6f7ef4016e9fd3c100e5e9f91a69641293e9c
berndengels <engels@f50.de>
2021-11-02 17:10:13 +0100

    stuff

--- database/migrations/testing/2021_10_20_204903_create_caravans_test_table.php
+++ database/migrations/testing/2021_10_20_204903_create_caravans_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCaravansTestTestTable extends Migration
+class CreateCaravansTestTable extends Migration
 {
     /**
      * Run the migrations.
