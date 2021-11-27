a68cbf7bf70cff2c8dc5d624d6d2e18fb1b5088b
berndengels <engels@f50.de>
2021-11-27 02:43:55 +0100

    stuff

--- resources/views/admin/customers/edit.blade.php
+++ resources/views/admin/customers/edit.blade.php
@@ -18,7 +18,8 @@
             <x-form-select name="customer_type" label="Typ" :options="$customerTypes" required />
             <x-form-input name="name" label="Name" required />
             <x-form-input type="email" name="email" label="Email" required />
-            <x-form-input type="password" name="password" label="Passwort" :bind="false" autocomplete="off" />
+            <x-form-input type="password" name="password" label="Passwort" :bind="false" autocomplete="off" readonly
+                onfocus="this.removeAttribute('readonly');" />
             <x-form-input type="password" name="password_confirmation" label="Passwort wiederholen" autocomplete="off" />
 
             <x-form-input name="fon" type="tel" label="Telefon" />
