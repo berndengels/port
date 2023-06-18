287fc309cbed31610cf4023954433f9062229977
btaberlin <technik@bta-weiterbildung.de>
2023-06-18 13:26:21 +0200

    doku

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,9 +5,9 @@ try {
 	require('bootstrap/js/dist/carousel');
 	require('bootstrap/js/dist/offcanvas');
 // require('bootstrap/js/dist/alert');
-// require('bootstrap/js/dist/button');
-// require('bootstrap/js/dist/collapse');
-// require('bootstrap/js/dist/dropdown');
+require('bootstrap/js/dist/button');
+require('bootstrap/js/dist/collapse');
+require('bootstrap/js/dist/dropdown');
 // require('bootstrap/js/dist/modal');
 // require('bootstrap/js/dist/popover');
 // require('bootstrap/js/dist/scrollspy');
